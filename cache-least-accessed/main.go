7c4b843c594106a029d8b628c338d45e54f410cb
vembry <airstrip99@gmail.com>
2023-07-09 07:08:27 +0700

    chore: simple init

--- cache-least-accessed/main.go
+++ cache-least-accessed/main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 )
 
+// node is the struct to store cache object
+type node struct {
+	Value string `json:"value"`
+}
+
 // cacheEngine is the cache engine
 type cacheEngine struct {
+	cap   int
+	cache map[string]*node
 }
 
 // NewCache is to initiate cache engine
 func NewCache(cap int) *cacheEngine {
-	return &cacheEngine{}
+	return &cacheEngine{
+		cap:   cap,
+		cache: make(map[string]*node),
+	}
 }
 
 // Set is to assign val to key onto the cache
 func (c *cacheEngine) Set(key string, val string) {
 	log.Printf("set '%s' with '%s'...", key, val)
 
+	// assign key-val to the cache
+	c.cache[key] = &node{
+		Value: val,
+	}
 }
 
 // Get is to get cache
 func (c *cacheEngine) Get(key string) string {
 	log.Printf("getting '%s'...", key)
-	// implement
+
+	// get from cache
+	if val, ok := c.cache[key]; ok {
+		return val.Value
+	}
+
+	// return empty when not found
 	return ""
 }
 
@@ -66,7 +88,7 @@ func main() {
 	cache.Set("key9", "val")
 	cache.Set("key10", "val")
 
-	// raw, _ := json.Marshal(cache.cache)
+	raw, _ := json.Marshal(cache.cache)
 	// raw, _ := json.MarshalIndent(cache.cache, "", " ")
-	// fmt.Printf("%s", string(raw))
+	fmt.Printf("%s", string(raw))
 }
